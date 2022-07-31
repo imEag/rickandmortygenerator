6d1bbd53058c36bb902b034efa26f6bf3a9e2587
Emmanuel <emma145@hotmail.es>
2022-07-30 20:57:22 -0500

    create query to get random character and create character component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-// Import everything needed to use the `useQuery` hook
 import { useQuery, gql } from '@apollo/client';
+
+import { Character } from './components/Character';
 
 export default function App() {
   return (
     <div>
       <h1>Rick And Morty Generator</h1>
+      <Character />
     </div>
   );
 }
--- src/queries/getRandomCharacter.js
+++ src/queries/getRandomCharacter.js
@@ -1,5 +1,37 @@
 import { gql } from '@apollo/client';
 
 export const GET_RANDOM_CHARACTER = gql`
-
+query randomCharacter{
+    randomCharacter {
+      id,
+      name,
+      species,
+      status,
+      type,
+      gender,
+      origin {
+        id,
+        name,
+        type,
+        dimension,
+        created
+      },
+      location {
+        id,
+        name,
+        type,
+        dimension,
+        created
+      },
+      image,
+      episode {
+        id,
+        name,
+        air_date,
+        episode,
+        created
+      },
+      created
+    }
+  }
 `;
